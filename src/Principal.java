f1c044382dcb2b2db3af96531ffe583b6bdc782b
MCPLUS-LUAN <luannascimento.microcamp@gmail.com>
2024-04-20 23:17:41 -0300

    Primeiro commit

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setIncluidoNoPlano(true);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8.5);
+        meuFilme.avalia(9.8);
+        meuFilme.avalia(10.0);
+        System.out.println(meuFilme.pegaMedia());
+    }
+
+
+}
+
