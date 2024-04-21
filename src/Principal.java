a7108a55ab736f8caef4304bceba703870930ea3
MCPLUS-LUAN <luannascimento.microcamp@gmail.com>
2024-04-21 00:46:19 -0300

    Implementando interface

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,7 @@
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -13,6 +16,26 @@ public class Principal {
         meuFilme.avalia(9.8);
         meuFilme.avalia(10.0);
         System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(380);
+
+        filtro.filtra(episodio);
+
     }
 
 
